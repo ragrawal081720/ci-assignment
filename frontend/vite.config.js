b015841c2c6b6a0ef230e4b34b215daaa0482654
ragrawal081720 <169162960+ragrawal081720@users.noreply.github.com>
2026-03-12 16:37:35 +0530

    kube.yaml and vite config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
+    // Allow current ELB hostname and future ELB hostnames in this AWS region.
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "abea0c534cf09460fabbd69584a81815-383028599.ap-south-1.elb.amazonaws.com",
+      ".ap-south-1.elb.amazonaws.com",
+    ],
   },
 });
