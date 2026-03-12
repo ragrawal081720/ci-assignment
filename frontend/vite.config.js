64ee86f153e22b27fa11416ded7d4d0136171c06
jatinbhalla <jatinbhalla18.jb@gmail.com>
2026-03-12 17:32:20 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,11 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    // Allow current ELB hostname and future ELB hostnames in this AWS region.
+    // Allow all AWS ELB hostnames
     allowedHosts: [
       "localhost",
       "127.0.0.1",
-      "abea0c534cf09460fabbd69584a81815-383028599.ap-south-1.elb.amazonaws.com",
-      ".ap-south-1.elb.amazonaws.com",
+      ".elb.amazonaws.com",
     ],
   },
 });
